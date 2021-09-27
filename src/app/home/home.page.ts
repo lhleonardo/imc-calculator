29f26dcd3af3e58f0c778835020a38d2cbdb0d3d
Leonardo Braz <lhleonardo05@gmail.com>
2021-09-27 00:26:01 -0300

    Classificação de valor do IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,20 +7,34 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   weight: number;
   height: number;
-  msgClassificação: string;
 
   constructor(private toastController: ToastController) {}
 
   isFormValid() {
-    return (this.height && this.weight && this.height > 0 && this.weight > 0);
+    return this.height && this.weight && this.height > 0 && this.weight > 0;
   }
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+
+    const msgClassificacao = this.getImcClassification(imc);
+
+    this.showMessage(`IMC = ${imc.toFixed(2)} - ${msgClassificacao}`);
+  }
+
+  private getImcClassification(imc: number): string {
+    if (imc < 18.5) return 'Magreza';
+    if (imc >= 18.5 && imc <= 24.9) return 'Normal';
+
+    if (imc >= 25 && imc <= 29.9) return 'Sobrepeso';
+
+    if (imc >= 30 && imc <= 39.9) return 'Obesidade';
+
+    if (imc > 40) return 'Obesidade Grave';
+
+    return 'Não classificado';
   }
 
   async showMessage(msg: string) {
@@ -29,17 +43,15 @@ export class HomePage {
       await this.toastController.dismiss();
     }
 
-    const toast = await this.toastController.create(
-      {
-        message: msg,
-        color: 'light',
-        buttons: [
-          {
-            icon: 'close'
-          }
-        ]
-      }
-    );
+    const toast = await this.toastController.create({
+      message: msg,
+      color: 'dark',
+      buttons: [
+        {
+          icon: 'close',
+        },
+      ],
+    });
     toast.present();
   }
 }
